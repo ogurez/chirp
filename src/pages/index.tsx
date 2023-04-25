60d5fdd57bd1b70984786ae806cfc5dee788b731
Viachaslau Lapitski <viachaslau.lapitski1@epam.com>
2023-04-25 21:24:41 +0100

    Update server routers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-	const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
 	const user = useUser();
+	const { data } = api.posts.getAll.useQuery();
+
+	console.log("DATA", data);
 
 	return (
 		<>
@@ -22,6 +23,11 @@ const Home: NextPage = () => {
 					<div>
 						{!user.isSignedIn && <SignInButton />}
 						{!!user.isSignedIn && <SignOutButton />}
+					</div>
+					<div>
+						{data?.map((post) => (
+							<div key={post.id}>{post.content}</div>
+						))}
 					</div>
 				</div>
 			</main>
